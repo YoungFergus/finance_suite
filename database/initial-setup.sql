d1d95761d396c510932ca739049bbc14e6c8cb8b
Erik Ferguson <eaferg510@gmail.com>
2024-02-19 15:31:37 -0500

    create transactions table

--- database/initial-setup.sql
+++ database/initial-setup.sql
@@ -1,6 +1,17 @@
 CREATE TABLE IF NOT EXISTS investment (
     investment_id INTEGER PRIMARY KEY AUTOINCREMENT
-    , investment_symbol TEXT
+    , ticker TEXT
     , created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     , updated_date TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    unique_id TEXT -- Unique_Id
+    , ticker TEXT -- Symbol
+    , position_date DATE -- Date
+    , action TEXT -- Action
+    , quantity NUMERIC -- Quantity
+    , price NUMERIC -- Price
+    , fees NUMERIC -- Fees & Comm
+);
+
